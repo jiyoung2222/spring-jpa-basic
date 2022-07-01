54a9be0bc654b8795decc3597d080eb083a3f934
jiyounglee <dlwldud913@naver.com>
2022-07-01 12:23:30 +0900

    6-값타입 매핑

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -10,15 +10,43 @@ public class Delivery extends BaseEntity{
     @GeneratedValue
     private Long ig;
 
-    private String city;
-
-    private String street;
-
-    private String zipcode;
+    @Embedded
+    private Address address;
 
     private DeliveryStatus status;
 
     @OneToOne(mappedBy = "delivery", fetch = LAZY)
     private Order order;
 
+    public Long getIg() {
+        return ig;
+    }
+
+    public void setIg(Long ig) {
+        this.ig = ig;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public DeliveryStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(DeliveryStatus status) {
+        this.status = status;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
 }
